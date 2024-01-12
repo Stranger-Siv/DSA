7c7ebb3216bf41ceba18735e0388a6db55c133f7
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-12 20:38:19 +0000

    REMOVE DUPLICATE

---
+++ ARRAY/EASY/ARR_02_REMOVE_DUPLICATE.java
@@ -0,0 +1,20 @@
+package ARRAY.EASY;
+
+public class ARR_02_REMOVE_DUPLICATE {
+
+    public static void myApproach(int arr[]){
+        int copy = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == copy ){
+                System.out.println();
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = {1,1,2,2,3,3,3};
+
+    }
+    
+}
