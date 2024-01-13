77adf3e8c8f5ab3097ec69db753260f790aedd9a
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-13 21:22:45 +0000

    UNION OF ARRAYS

---
+++ ARRAY/EASY/ARRAY/EASY/ARR_09_FIND_UNION.java
@@ -0,0 +1,22 @@
+public class ARR_09_FIND_UNION {
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void union(int arr1[],int arr2[]){
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr1[]={1,2,3,4,5};
+        int arr2[]={4,5,6,7,8};
+        union(arr1, arr2, 10);
+    }
+}
