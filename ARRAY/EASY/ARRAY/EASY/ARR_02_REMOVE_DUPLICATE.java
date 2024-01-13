b0988395dd6ecd0f1d1f46692f0fc5cdf5bf919f
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-13 17:07:31 +0000

    Remove Duplicate

---
+++ ARRAY/EASY/ARRAY/EASY/ARR_02_REMOVE_DUPLICATE.java
@@ -0,0 +1,35 @@
+public class ARR_02_REMOVE_DUPLICATE {
+
+    public static int myApproach(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if(arr[i] != arr[j]){
+                    i++;
+                    arr[i] = arr[j];
+                }
+            }
+            return i+1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 1, 2, 2, 3, 3, 3 };
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println();
+
+        int k = myApproach(arr);
+        for(int i=0;i<k;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        
+
+    }
+
+}
