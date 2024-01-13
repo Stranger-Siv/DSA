a5a0036afedabf478f4fac71397e54e719558ea9
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-13 19:43:03 +0000

    ROTATE BY N PLACES

--- ARRAY/EASY/ARRAY/EASY/ARR_06_ROTATE_BY_N.java
+++ ARRAY/EASY/ARRAY/EASY/ARR_06_ROTATE_BY_N.java
@@ -34,7 +34,33 @@ public class ARR_06_ROTATE_BY_N {
         }
     }
 
+    public static void reverse(int arr[],int start,int end){
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+          }
+    }
 
+    public static void Rotateeletoright(int[] arrrr, int n, int k) {
+        // Reverse first n-k elements
+        reverse(arrrr, 0, n - k - 1);
+        // Reverse last k elements
+        reverse(arrrr, n - k, n - 1);
+        // Reverse whole array
+        reverse(arrrr, 0, n - 1);
+      }
+
+      public static void Rotateeletoleft(int[] ar, int n, int k) {
+        // Reverse first k elements
+        reverse(ar, 0, k - 1);
+        // Reverse last n-k elements
+        reverse(ar, k , n - 1);
+        // Reverse whole array
+        reverse(ar, 0, n - 1);
+      }
 
     public static void main(String[] args) {
 
@@ -43,10 +69,22 @@ public class ARR_06_ROTATE_BY_N {
         print(arr);
 
         System.out.println();
-        
+
         int arrr[]={3,7,8,9,10,11};
         rotateLeft(arrr, 3, 6);
         print(arrr);
+
+        System.out.println();
+
+        int arrrr[]={1,2,3,4,5,6,7};
+        Rotateeletoright(arrrr, 7, 2);
+        print(arrrr);
+
+        System.out.println();
+
+        int ar[]={3,7,8,9,10,11};
+        Rotateeletoleft(ar, 6, 3);
+        print(ar);
 
     }
 }
