71167be2d6de2f01d73d9afead738adddd46c570
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-15 17:24:59 +0000

    MAXIMUM CONSECUTIVE ONES

---
+++ ARRAY/EASY/ARRAY/EASY/ARR_11_MAX_CONSECUTIVE_ONES.java
@@ -0,0 +1,26 @@
+public class ARR_11_MAX_CONSECUTIVE_ONES {
+
+    public static int maxCount(int arr[]){
+        int count =0;
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+    }
+       
+
+
+    public static void main(String[] args) {
+        // int arr[]={1,1,0,1,1,1};
+        int arr[]={1,1,1,0,1,1,1,1,1,0,1};
+        int ans=maxCount(arr);
+        System.out.println(ans);
+    }
+}
