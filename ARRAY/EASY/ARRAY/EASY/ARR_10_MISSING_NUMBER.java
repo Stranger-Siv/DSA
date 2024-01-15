4743194badc0a62a33d9aabdd732000cadbbfbd5
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-15 16:41:58 +0000

    MISSING NUMBER

--- ARRAY/EASY/ARRAY/EASY/ARR_10_MISSING_NUMBER.java
+++ ARRAY/EASY/ARRAY/EASY/ARR_10_MISSING_NUMBER.java
@@ -1,21 +1,49 @@
 import java.util.ArrayList;
 
 public class ARR_10_MISSING_NUMBER {
-
-    public static void missing(int arr[]){
-        ArrayList<Integer>a2=new ArrayList<Integer>();
-        for(Integer element:a2 ){
-            element.add(arr);
+    public static int missingBrute(int arr[]){
+        for(int i=1;i<=arr.length;i++){
+            int flag=0;
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]==i){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag == 0) return i;
         }
-        
-        System.out.println();
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+        return -1;
     }
+
+    public static int sum_1_N(int arr[]){
+        int n=arr.length+1;
+        int sum = (n*(n+1))/2;
+
+        return sum;
+    }
+    
+    public static int arrSum(int arr[]){
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+
+        return sum;
+        
+    }
+
     public static void main(String[] args) {
         int arr[]={1,2,4,5};
-        missing(arr);
+        int ans=missingBrute(arr);
+        System.out.println("The missing number is "+ans);
+
+        System.out.println();
+
+        int ans1=sum_1_N(arr);
+        int ans2=arrSum(arr);
+        int answer = ans1-ans2;
+        System.out.println("The missing number is "+answer);
+        
+        
     }
 }
