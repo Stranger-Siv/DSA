d68d90de26dbd98b3486628f6076cce37ca37472
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-15 06:56:14 +0000

    UNION OF TWO ARRAY

--- ARRAY/EASY/ARRAY/EASY/ARR_09_FIND_UNION.java
+++ ARRAY/EASY/ARRAY/EASY/ARR_09_FIND_UNION.java
@@ -1,22 +1,39 @@
+import java.util.*;
+
 public class ARR_09_FIND_UNION {
-
-    public static void print(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    public static ArrayList<Integer> union(int a1[],int a2[]){
+        HashSet <Integer> s = new HashSet <>();
+        ArrayList <Integer> a3 = new ArrayList <Integer>();
+        for(int i=0;i<a1.length;i++){
+            s.add(a1[i]);
         }
-    }
-
-    public static void union(int arr1[],int arr2[]){
-        for(int i=0;i<arr1.length;i++){
-            for(int j=0;j<arr2.length;j++){
-                
-            }
+        for(int i=0;i<a2.length;i++){
+            s.add(a2[i]);
         }
+        for(int ans:s){
+            a3.add(ans);
+        }
+        return a3;
 
     }
     public static void main(String[] args) {
-        int arr1[]={1,2,3,4,5};
-        int arr2[]={4,5,6,7,8};
-        union(arr1, arr2, 10);
+       int a1[]={1,2,3,4,5};
+       int a2[]={4,5,6,7,8};
+       for(int val:a1){
+        System.out.print(val+" ");
+       }
+
+       System.out.println();
+
+       for(int val:a2){
+        System.out.print(val+" ");
+       }
+       
+       System.out.println();
+
+       ArrayList <Integer> a3=union(a1,a2);
+       for(int val:a3){
+        System.out.print(val+" ");
+       }
     }
 }
