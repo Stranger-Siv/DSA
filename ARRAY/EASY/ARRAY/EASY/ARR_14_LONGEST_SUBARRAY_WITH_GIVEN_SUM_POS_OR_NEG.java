704e2611e2161ee716c392a6149d9c824a537bd5
Sivram Pradhan <123907114+Stranger-Siv@users.noreply.github.com>
2024-01-16 04:50:04 +0000

    LONGEST SUBARRAY WITH GIVEN SUM K (WITH POSITIVE AND NEGATIVE)

--- ARRAY/EASY/ARRAY/EASY/ARR_14_LONGEST_SUBARRAY_WITH_GIVEN_SUM_POS_OR_NEG.java
+++ ARRAY/EASY/ARRAY/EASY/ARR_14_LONGEST_SUBARRAY_WITH_GIVEN_SUM_POS_OR_NEG.java
@@ -1,5 +1,23 @@
 public class ARR_14_LONGEST_SUBARRAY_WITH_GIVEN_SUM_POS_OR_NEG {
-    public static void main(String[] args) {
 
+    public static int bruteApproach(int arr[], int k) {
+        int n = arr.length;
+
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == k) {
+                    len = Math.max(len, j - i + 1);
+                }
+            }
+        }
+        return len;
+    }
+    public static void main(String[] args) {
+        int arr[]={-1,1,1};
+        int ans=bruteApproach(arr, 1);
+        System.out.println(ans);
     }
 }
